0389314e9e0698b73b92fb14417403356f7ef4d5
UNICDEB <doloidebabrat99@gmail.com>
2024-05-15 16:10:14 +0530

    Added

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import streamlit as st
-st.title("Simple ")
-st.sidebar.selectbox("Pick a option", ("a", 'b',"c"))
+st.title("Resturant Menu Generator")
+cuisine = st.sidebar.selectbox("Pick a option", ("Indian", 'Mexican',"Italian", "Spanish", "French", "Duch"))
+
+def generate_resturant_name_and_item(cuisine):
+    return{
+        'resturant_name':"Bengali Dhaba",
+        'menu_item':'Samosa, Paneer Tikka, Nun'
+    }
+    
+if cuisine:
+    response = generate_resturant_name_and_item(cuisine)
+    st.header(response['resturant_name'])
+    menu_items = response['menu_item'].split(",")
+    
+    for item in menu_items:
+        st.write("-", item)
